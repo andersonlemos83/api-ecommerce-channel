d7d85b3f24e396ea333736498030050d6608580d
Anderson Lemos Camelo <andersonlemos83@gmail.com>
2025-02-17 20:44:28 -0300

    Commit inicial do projeto.

---
+++ core/src/main/java/br/com/alc/ecommerce/channel/core/EcommerceChannelCoreApplication.java
@@ -0,0 +1,7 @@
+package br.com.alc.ecommerce.channel.core;
+
+public class EcommerceChannelCoreApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ infrastructure/src/main/java/br/com/alc/ecommerce/channel/infrastructure/EcommerceChannelInfrastructureApplication.java
@@ -0,0 +1,7 @@
+package br.com.alc.ecommerce.channel.infrastructure;
+
+public class EcommerceChannelInfrastructureApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
