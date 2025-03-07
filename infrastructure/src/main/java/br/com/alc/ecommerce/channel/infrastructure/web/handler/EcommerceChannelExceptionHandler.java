59653c7caa3855a30b854d332017273eab6857b2
Anderson Lemos Camelo <andersonlemos83@gmail.com>
2025-03-06 21:02:36 -0300

    Evolui todas as dependências do projeto.

--- infrastructure/src/main/java/br/com/alc/ecommerce/channel/infrastructure/web/handler/EcommerceChannelExceptionHandler.java
+++ infrastructure/src/main/java/br/com/alc/ecommerce/channel/infrastructure/web/handler/EcommerceChannelExceptionHandler.java
@@ -3,31 +3,75 @@ package br.com.alc.ecommerce.channel.infrastructure.web.handler;
 import br.com.alc.ecommerce.channel.core.exception.OrderNotFoundException;
 import br.com.alc.ecommerce.channel.core.exception.PeriodInvalidException;
 import br.com.alc.ecommerce.channel.infrastructure.dto.error.ErrorResponseDto;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
+import java.text.MessageFormat;
+
+import static br.com.alc.ecommerce.channel.core.util.ObjectMapperUtil.generateJson;
+import static java.util.Comparator.naturalOrder;
+import static java.util.stream.Collectors.joining;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@Log4j2
 @ControllerAdvice
-@ConditionalOnProperty(name = "app.handle.enable", havingValue = "true", matchIfMissing = true)
-public class EcommerceChannelExceptionHandler extends AbstractExceptionHandler {
+public class EcommerceChannelExceptionHandler {
 
     @ExceptionHandler(WebExchangeBindException.class)
     public Mono<ResponseEntity<ErrorResponseDto>> handleWebExchangeBindException(WebExchangeBindException exception) {
-        return super.handleGenericMethodArgumentNotValidException(exception.getBindingResult());
+        return Mono.just(exception.getBindingResult())
+                .map(this::joiningMessages)
+                .map(this::buildBadRequestErrorResponseDto)
+                .map(this::buildBadRequestResponseEntity);
     }
 
-    @Override
     @ExceptionHandler(OrderNotFoundException.class)
     public Mono<ResponseEntity<ErrorResponseDto>> handleOrderNotFoundException(OrderNotFoundException exception) {
-        return super.handleOrderNotFoundException(exception);
+        return handeBusinessException(exception);
     }
 
-    @Override
     @ExceptionHandler(PeriodInvalidException.class)
     public Mono<ResponseEntity<ErrorResponseDto>> handlePeriodInvalidException(PeriodInvalidException exception) {
-        return super.handlePeriodInvalidException(exception);
+        return handeBusinessException(exception);
+    }
+
+    private String joiningMessages(BindingResult bindingResult) {
+        return bindingResult
+                .getFieldErrors()
+                .stream()
+                .map(this::buildMessage)
+                .sorted(naturalOrder())
+                .distinct()
+                .collect(joining(", ", "", "."));
+    }
+
+    private String buildMessage(FieldError fieldError) {
+        return MessageFormat.format("O campo {0} {1}", fieldError.getField(), fieldError.getDefaultMessage());
+    }
+
+    private Mono<ResponseEntity<ErrorResponseDto>> handeBusinessException(Exception exception) {
+        return Mono.just(exception)
+                .map(Exception::getMessage)
+                .map(this::buildBadRequestErrorResponseDto)
+                .map(this::buildBadRequestResponseEntity);
+    }
+
+    private ErrorResponseDto buildBadRequestErrorResponseDto(String message) {
+        ErrorResponseDto errorResponseDto = ErrorResponseDto.builder()
+                .httpStatus(BAD_REQUEST)
+                .message(message)
+                .build();
+        log.debug("Outgoing from AbstractExceptionHandler: {}", generateJson(errorResponseDto));
+        return errorResponseDto;
+    }
+
+    private ResponseEntity<ErrorResponseDto> buildBadRequestResponseEntity(ErrorResponseDto errorResponseDto) {
+        return ResponseEntity.status(BAD_REQUEST).body(errorResponseDto);
     }
 }
--- infrastructure/src/test/java/br/com/alc/ecommerce/channel/infrastructure/cucumber/SpringContextStepDefs.java
+++ infrastructure/src/test/java/br/com/alc/ecommerce/channel/infrastructure/cucumber/SpringContextStepDefs.java
@@ -17,7 +17,6 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 
 import java.lang.reflect.Type;
 import java.util.LinkedHashMap;
@@ -28,7 +27,6 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @ActiveProfiles("test")
 @AutoConfigureWebTestClient
 @CucumberContextConfiguration
-@TestPropertySource(properties = "app.handle.enable=false")
 @SpringBootTest(classes = EcommerceChannelInfrastructureApplication.class, webEnvironment = DEFINED_PORT)
 @ContextConfiguration(classes = {EcommerceChannelInfrastructureApplication.class, EcommerceChannelInfrastructureConfig.class, RabbitConfig.class, WireMockConfig.class, GreenMailConfig.class})
 public class SpringContextStepDefs extends StepDefs {
